8bb1ba53d8a1bed61f6996b7911605b43caf98c6
shiyue <86143925+xiaoyuer0408@users.noreply.github.com>
2023-05-17 06:22:24 +0800

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -4,9 +4,21 @@ from database import save_message, user_sessions
 import openai
 
 def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Hello, I am a chat bot. How can I assist you today?')
+    """
+    Telegram 机器人的 /start 命令处理程序
+    参数：
+        update - 更新对象，包含有关当前更新的信息
+        context - 回调上下文对象，用于处理与 Telegram API 的交互
+    """
+    update.message.reply_text('你好，我是一个聊天机器人。今天我能帮你做什么？')
 
 def respond(update: Update, context: CallbackContext) -> None:
+    """
+    Telegram 机器人的消息处理程序
+    参数：
+        update - 更新对象，包含有关当前更新的信息
+        context - 回调上下文对象，用于处理与 Telegram API 的交互
+    """
     message = update.message.text
     chat_id = update.effective_chat.id
     user_id = update.effective_user.id
@@ -28,10 +40,10 @@ def respond(update: Update, context: CallbackContext) -> None:
             generate_image(text)
             context.bot.send_photo(chat_id=chat_id, photo=open("generated_image.png", "rb"))
         else:
-            context.bot.send_message(chat_id=chat_id, text="Please provide some text after the command.")
+            context.bot.send_message(chat_id=chat_id, text="请在命令后提供一些文本。")
     else:
-        if "Who are you?" in message:
-            response = "I am a friendly chat bot. I'm here to assist you with any questions you have. How can I help you today?"
+        if "你是谁？" in message:
+            response = "我是一个友好的聊天机器人。我在这里帮助您解答任何问题。今天我能帮你做什么？"
         else:
             response = openai.ChatCompletion.create(
                 model="gpt-4",
