45b612409324445226c979514dcd4025a4039e50
shiyue <86143925+xiaoyuer0408@users.noreply.github.com>
2023-05-17 04:19:09 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from .config import TELEGRAM_TOKEN
+from .handlers import start, respond
+
+def main() -> None:
+    updater = Updater(token=TELEGRAM_TOKEN)
+
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, respond))
+
+    updater.start_polling()
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
