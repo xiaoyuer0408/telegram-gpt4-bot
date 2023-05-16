07885dab3db7354c92958f9e60af7c4aac6fb837
shiyue <86143925+xiaoyuer0408@users.noreply.github.com>
2023-05-17 05:36:34 +0800

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,11 +1,7 @@
-from telegram import Update, MessageHandler, Filters, CallbackContext
-import openai
-import time
-import json
-from PIL import Image, ImageDraw, ImageFont
-from config import openai, TELEGRAM_TOKEN
+from telegram import Update, CallbackContext
 from image_gen import generate_image
-from database import save_message, get_messages, user_sessions
+from database import save_message, user_sessions
+import openai
 
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Hello, I am a chat bot. How can I assist you today?')
@@ -15,27 +11,21 @@ def respond(update: Update, context: CallbackContext) -> None:
     chat_id = update.effective_chat.id
     user_id = update.effective_user.id
 
-    save_message(chat_id, user_id, message)  # 保存用户消息到数据库
+    save_message(chat_id, user_id, message)
 
-    # 如果是在群聊中，检查消息是否是由机器人自身发送的
     if update.message.from_user.is_bot:
         return
 
-    # 如果用户是新的，创建一个新的会话ID
     if chat_id not in user_sessions:
         user_sessions[chat_id] = f"{chat_id}-{time.time()}"
 
     session_id = user_sessions[chat_id]
 
     if message.startswith("/generate_image"):
-        # 获取要生成图片的文字内容
         text = message.replace("/generate_image", "").strip()
 
-        if text:  # 添加了输入验证
-            # 调用生成图片的函数
+        if text:
             generate_image(text)
-
-            # 发送生成的图片给用户
             context.bot.send_photo(chat_id=chat_id, photo=open("generated_image.png", "rb"))
         else:
             context.bot.send_message(chat_id=chat_id, text="Please provide some text after the command.")
@@ -43,9 +33,8 @@ def respond(update: Update, context: CallbackContext) -> None:
         if "Who are you?" in message:
             response = "I am a friendly chat bot. I'm here to assist you with any questions you have. How can I help you today?"
         else:
-            # 使用 Chat API 进行会话
             response = openai.ChatCompletion.create(
-                model="gpt-4",  # 更改为新的模型
+                model="gpt-4",
                 messages=[
                     {"role": "system", "content": "You are a helpful assistant."},
                     {"role": "user", "content": message},
@@ -53,5 +42,4 @@ def respond(update: Update, context: CallbackContext) -> None:
                 session_id=session_id
             ).choices[0].message['content']
 
-        # 发送回复给用户
         update.message.reply_text(response)
