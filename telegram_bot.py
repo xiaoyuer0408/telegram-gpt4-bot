f571ec5fbe7e297a0e485442aa9f9607775089f2
shiyue <86143925+xiaoyuer0408@users.noreply.github.com>
2023-05-16 18:21:59 +0800

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,18 +1,37 @@
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 import openai
+import os
 
 # 配置你的 OpenAI 和 Telegram Token
-openai.api_key = 'YOUR_OPENAI_API_KEY'
+openai.api_key = os.getenv('OPENAI_API_KEY')
 TELEGRAM_TOKEN = 'YOUR_TELEGRAM_TOKEN'
+
+# 为每个用户创建一个会话ID以管理多轮对话
+user_sessions = {}
 
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Hello, I am a chat bot. How can I assist you today?')
 
 def respond(update: Update, context: CallbackContext) -> None:
     message = update.message.text
-    response = openai.Completion.create(engine="text-davinci-002", prompt=message, max_tokens=150)
-    update.message.reply_text(response.choices[0].text.strip())
+    chat_id = update.effective_chat.id
+
+    # 如果用户是新的，创建一个新的会话ID
+    if chat_id not in user_sessions:
+        user_sessions[chat_id] = f"{chat_id}-{time.time()}"
+
+    session_id = user_sessions[chat_id]
+    # 使用 Chat API 进行会话
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",  # 更换为你选择的模型
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": message},
+        ],
+        session_id=session_id,
+    )
+    update.message.reply_text(response['choices'][0]['message']['content'])
 
 def main() -> None:
     updater = Updater(token=TELEGRAM_TOKEN)
