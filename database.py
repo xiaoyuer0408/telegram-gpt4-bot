4cd631b7b3660eac9b86d028ee64ef2eff7e1ba7
shiyue <86143925+xiaoyuer0408@users.noreply.github.com>
2023-05-17 08:42:30 +0800

    Update database.py

--- database.py
+++ database.py
@@ -19,28 +19,34 @@ def close_db_connection(conn):
     """
     conn.close()
 
-def save_message(cursor, chat_id, user_id, message):
+def save_message(chat_id, user_id, message):
     """
     将消息保存到数据库
     参数：
-        cursor - 数据库游标对象
         chat_id - 聊天 ID
         user_id - 用户 ID
         message - 用户发送的消息
     """
+    conn = get_db_connection()
+    cursor = conn.cursor()
     cursor.execute('INSERT INTO user_messages (chat_id, user_id, message) VALUES (?, ?, ?)', (chat_id, user_id, message))
+    conn.commit()
+    close_db_connection(conn)
 
-def get_messages(cursor, chat_id, limit=None):
+def get_messages(chat_id, limit=None):
     """
     获取指定聊天 ID 的消息，按时间倒序排序
     参数：
-        cursor - 数据库游标对象
         chat_id - 聊天 ID
         limit - 可选参数，限制返回的消息数量
     返回：查询结果列表
     """
+    conn = get_db_connection()
+    cursor = conn.cursor()
     query = 'SELECT * FROM user_messages WHERE chat_id = ? ORDER BY timestamp DESC'
     if limit is not None:
         query += ' LIMIT {}'.format(limit)
     cursor.execute(query, (chat_id,))
-    return cursor.fetchall()
+    result = cursor.fetchall()
+    close_db_connection(conn)
+    return result
