39feefb31ae0a496f017c81438d05c8ba8de14a5
shiyue <86143925+xiaoyuer0408@users.noreply.github.com>
2023-05-17 08:34:31 +0800

    Update image_gen.py

--- image_gen.py
+++ image_gen.py
@@ -1,10 +1,11 @@
 from PIL import Image, ImageDraw, ImageFont
 
-def generate_image(text, font_file="arial.ttf", image_size=(500, 200)):
+def generate_image(text, output_filename="generated_image.png", font_file="arial.ttf", image_size=(500, 200)):
     """
     生成包含给定文本的图像
     参数：
         text - 要在图像上显示的文本
+        output_filename - 可选参数，生成图像的文件名，默认为 "generated_image.png"
         font_file - 可选参数，字体文件名，默认为 "arial.ttf"
         image_size - 可选参数，生成图像的尺寸，默认为 (500, 200)
     """
@@ -13,6 +14,5 @@ def generate_image(text, font_file="arial.ttf", image_size=(500, 200)):
     font = ImageFont.truetype(font_file, 20)
     draw.text((10, 10), text, fill=(0, 0, 0), font=font)
 
-    with open("generated_image.png", "wb") as f:
+    with open(output_filename, "wb") as f:
         image.save(f)
-
